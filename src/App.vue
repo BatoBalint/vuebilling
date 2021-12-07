07df437a8456a66609ebf9fb5fbb9e7c8e038712
BatoBalint <balint.bato@gmail.com>
2021-12-07 16:48:45 +0100

    lehet hozzaadni, torolni

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-8 mx-auto">
+          <div id="tablazat">
+            <table class="table table-dark text-light">
+              <tr>
+                <th>Nev</th>
+                <th>Ar</th>
+                <th>Mennyiseg</th>
+                <th>Operations</th>
+              </tr>
+              <tr v-for="item in rows" v-bind:key="item.title">
+                <td>{{ item.title }}</td>
+                <td>{{ item.price }}</td>
+                <td>{{ item.quantity }}</td>
+                <td>
+                  <button class="btn btn-danger bg-danger" v-on:click="deleteElem(item.title)">X</button>
+                  <button class="btn btn-warning bg-warning" v-on:click="editElem">Edit</button>
+                </td>
+              </tr>
+                <tr>
+                  <td><input class="form-control text-light bg-dark" type="text" placeholder="Nev" v-model="nameInput"></td>
+                  <td><input class="form-control text-light bg-dark" type="text" placeholder="Ar" v-model="priceInput"></td>
+                  <td><input class="form-control text-light bg-dark" type="text" placeholder="Mennyiseg" v-model="qtyInput"></td>
+                  <td><input class="btn btn-success" type="submit" value="Add" v-on:click="add"></td>
+                </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    add() {
+      if (this.nameInput !== "" && this.priceInput !== "" && this.qtyInput !== "") {
+                var newLine = {
+                    title: this.nameInput,
+                    price: this.priceInput,
+                    quantity: this.qtyInput
+                };
+                this.rows.push(newLine);
+                this.nameInput = "";
+                this.priceInput = "";
+                this.qtyInput = "";
+            } else {
+                alert("Egyik mezo sem maradthat ures");
+            }
+    },
+    deleteElem(title) {
+      for (let row of this.rows) {
+        if (row.title == title) {
+          this.rows.splice(this.rows.indexOf(row), 1);
+        }
+      }
+    },
+    editElem() {
+      alert("test");
+    }
   },
   data() {
     return {
+      "nameInput": "",
+      "priceInput": "",
+      "qtyInput": "",
       rows: [
         {
           title: 'Kerék',
@@ -43,12 +98,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  tr {
+    margin: 10px 0;
+  }
+  button {
+    margin-right: 10px;
+  }
 </style>

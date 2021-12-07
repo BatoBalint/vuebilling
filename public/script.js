75fd746b869e93bd7515be4e091a39303585c701
BatoBalint <balint.bato@gmail.com>
2021-12-07 14:43:32 +0100

    design resz megvan meg a listazas

---
+++ public/script.js
@@ -0,0 +1,28 @@
+let app = new Vue({
+    el: '#tablazat',
+    data: {
+      message: "Kacsa",
+      rows: [
+        {
+          title: 'Kerék',
+          price: 100,
+          quantity: 12
+        },
+        {
+          title: 'Teleszkóp',
+          price: 1000,
+          quantity: 300
+        },
+        {
+          title: 'Kormány',
+          price: 230,
+          quantity: 5
+        },
+        {
+          title: 'Ajtó',
+          price: 45120,
+          quantity: 321
+        },
+      ]
+    }
+  })
